99d69d5377c7f2464f82707aafce92bc3becdcca
anacorrea791 <correaanaclara898@gmail.com>
2025-09-12 08:39:43 -0300

    Adiciona suporte a gestão de médicos, pacientes e agendas
    Foram realizadas as seguintes alterações principais:
    
    - Adicionada a dependência `Microsoft.VisualStudio.Web.CodeGeneration.Design` (v8.0.7) ao projeto.
    - Criados controladores para as entidades `Medico`, `Paciente`, `Agenda` e `Especialidade`, com suporte a operações CRUD.
    - Adicionados os modelos de dados `Medico`, `Paciente`, `Agenda` e `Especialidade`, com propriedades e relacionamentos.
    - Criadas migrações para refletir as novas tabelas e relações no banco de dados.
    - Atualizado o `ApplicationDbContext` para incluir os `DbSet` das novas entidades e configurar os mapeamentos no método `OnModelCreating`.
    - Criadas views para gerenciar as entidades, incluindo páginas `Create`, `Edit`, `Delete`, `Details` e `Index`.
    - Atualizado o `_Layout.cshtml` para incluir links de navegação para as novas páginas.
    - Removida a dependência entre a tabela `Pacientes` e a tabela de usuários (`AspNetUsers`).
    - Atualizada a versão do Entity Framework para `8.0.12`.
    
    Essas mudanças introduzem um sistema completo para gerenciar médicos, pacientes, especialidades e agendamentos de consultas, com suporte a validações e integridade referencial.

---
+++ Models/Agenda.cs
@@ -0,0 +1,13 @@
+﻿namespace AgendaMedica.Models
+{
+    public class Agenda
+    {
+        public Guid AgendaId { get; set; } // chave primaria PK
+        public Guid PacienteId { get; set; } // chave estrangeira FK
+        public Paciente? Paciente { get; set; } // objeto referente a chave acima
+        public Guid MedicoId { get; set; } // chave estrangeira FK
+        public Medico? Medico { get; set; } // objeto referente a chave acima
+        public DateTime DataConsulta { get; set; }
+        public string? Status { get; set; }
+    }
+}
